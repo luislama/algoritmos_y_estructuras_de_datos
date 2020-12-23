4c3adb88d620d31a4c99e9b5674d966cb74e8ec2
Sunjing Lama <luislama@espol.edu.ec>
2020-12-23 07:55:30 -0500

    Upd bubble_sort

--- algoritmos/22_bubble_sort/bubble_sort.py
+++ algoritmos/22_bubble_sort/bubble_sort.py
@@ -6,20 +6,25 @@
 elements = [2,7,1,5,0,4,3,8,-1,2,4]
 num_elements = len(elements)
 
+
 def bubble_sort(arreglo):
     ordered = False
     result =  arreglo
     ronda = 1
     while not ordered:
         ordered = True
-        print(result)
         for i in range(0, num_elements - ronda):
             if result[i] > result[i+1]:
                 result[i], result[i+1] = result[i+1], result[i]
                 ordered = False
+        print("".join(["ronda ", str(ronda), ":"]))
+        print(result)
         ronda += 1
     return result
 
 
+print("arreglo: ")
+print(elements)
+print("\n")
+ordered_elements = bubble_sort(elements)
 
-print(bubble_sort(elements))
