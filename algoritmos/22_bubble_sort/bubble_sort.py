8e95e8966cc1f13a7127277448be711f159dc1b5
Sunjing Lama <luislama@espol.edu.ec>
2020-12-24 04:56:51 -0500

    Upd descripcion

--- algoritmos/22_bubble_sort/bubble_sort.py
+++ algoritmos/22_bubble_sort/bubble_sort.py
@@ -1,6 +1,20 @@
 '''
-    Ordenar arreglos segun bubble_sort
+    Se recorre el arreglo elemento por elemento comparandolo con el siguiente
+    Si el elemento es mayor al siguiete, estos se intercambian y se continua con el siguiente elemento
+    De esta manera se hacen varios recorridos al arreglo
+    Luego de terminar cada recorrido, de haber realizado al menos 1 intercambio durante este, entonces se debe hacer otro mas
+
+    El arreglo asegura tomar el elemento mayor y colocarlo al final de manera ordenada en el arreglo en cada iteracion
+
     [2,7,1,5,0,4,3,8,-1,2,4]
+    r1: [2,1,5,0,4,3,7,-1,2,4,8]
+    r2: [1,2,0,4,3,5,-1,2,4,7,8]
+    r3: [1,0,2,3,4,-1,2,4,5,7,8]
+    r4: [0,1,2,3,-1,2,4,4,5,7,8]
+    r5: [0,1,2,-1,2,3,4,4,5,7,8]
+    r6: [0,1,-1,2,2,3,4,4,5,7,8]
+    r7: [0,-1,1,2,2,3,4,4,5,7,8]
+    r8: [-1,0,1,2,2,3,4,4,5,7,8]
 '''
 
 elements = [2,7,1,5,0,4,3,8,-1,2,4]
--- algoritmos/23_selection_sort/selection_sort.py
+++ algoritmos/23_selection_sort/selection_sort.py
@@ -1,6 +1,22 @@
 '''
-    Ordenar arreglos segun selection_sort
+    Se recorre el arrego elemento por elemento, buscando la posicion del menor elemento entre los siguientes que faltan por recorrer
+    Luego de terminar de revisar los elementos siguientes, y obtener la posicion del menor de ellos, comparo el elemento en esa posicion con el de la posicion actual
+    De ser mayor el elemento en la posicion actual, se intercambian estos elementos
+    Al terminar el recorrido, elemento por elemento, termina el algoritmo
+
+    El arreglo asegura tomar el elemento menor y colocarlo al principio de manera ordenada en el arreglo en cada iteracion
+
     [2,7,1,5,0,4,3,8,-1,2,4]
+    r1: [-1,7,1,5,0,4,3,8,2,2,4]
+    r2: [-1,0,1,5,7,4,3,8,2,2,4]
+    r3: [-1,0,1,5,7,4,3,8,2,2,4]
+    r4: [-1,0,1,2,7,4,3,8,5,2,4]
+    r5: [-1,0,1,2,2,4,3,8,5,7,4]
+    r6: [-1,0,1,2,2,3,4,8,5,7,4]
+    r7: [-1,0,1,2,2,3,4,8,5,7,4]
+    r8: [-1,0,1,2,2,3,4,4,5,7,8]
+    r9: [-1,0,1,2,2,3,4,4,5,7,8]
+    r10: [-1,0,1,2,2,3,4,4,5,7,8]
 '''
 
 elements = [2,7,1,5,0,4,3,8,-1,2,4]
